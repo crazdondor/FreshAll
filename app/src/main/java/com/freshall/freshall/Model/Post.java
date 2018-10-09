2b7b8c2172fd5b15f75f8f7da89690c913210ab3
qbingham <quinbingham@gmail.com>
2018-10-08 21:35:20 -0700

    Updated Model Package with fields, constructors, getters/setters, and method declarations.

--- app/src/main/java/com/freshall/freshall/Model/Post.java
+++ app/src/main/java/com/freshall/freshall/Model/Post.java
@@ -7,12 +7,14 @@ public class Post {
     Item postItem;
     User postUser;
     Date postTime;
+    Date updateTime;
 
-    public Post(String postText, Item postItem, User postUser, Date postTime) {
+    public Post(String postText, Item postItem, User postUser, Date postTime, Date updateTime) {
         this.postText = postText;
         this.postItem = postItem;
         this.postUser = postUser;
         this.postTime = postTime;
+        this.updateTime = updateTime;
     }
 
     public String getPostText() {
@@ -45,6 +47,14 @@ public class Post {
 
     public void setPostTime(Date postTime) {
         this.postTime = postTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
     }
 
     //methods

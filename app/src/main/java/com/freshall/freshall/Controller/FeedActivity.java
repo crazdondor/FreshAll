ed49f6826c43df41cabf85a9d8926d9926dc4017
Bennett Falkenberg <bennettfalkenberg@gmail.com>
2018-11-26 15:08:09 -0800

    First commit for Manual merging

--- app/src/main/java/com/freshall/freshall/Controller/FeedActivity.java
+++ app/src/main/java/com/freshall/freshall/Controller/FeedActivity.java
@@ -1,5 +1,7 @@
 package com.freshall.freshall.Controller;
 
+import com.freshall.freshall.Model.Post;
+
 import android.app.Activity;
 import android.content.Intent;
 
@@ -13,7 +15,11 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.support.design.widget.BottomNavigationView;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.freshall.freshall.R;
@@ -32,17 +38,22 @@ import java.util.List;
 public class FeedActivity extends AppCompatActivity {
 
     static final int SIGN_IN_REQUEST = 1;
+    static final int NEW_ITEM_REQUEST = 2;
 
     String userName = "Anonymous";
-    List<ChatMessage> chatMessageList;
-    ArrayAdapter<ChatMessage> arrayAdapter;
     ListView listView;
 
     FirebaseDatabase mFirebaseDatabase;
-    DatabaseReference mMessagesDatabaseReference;
-    ChildEventListener mMessagesChildEventListener;
+    DatabaseReference mPostDatabaseReference;
+    ChildEventListener mPostChildEventListener;
     FirebaseAuth mFirebaseAuth;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private TextView mTextMessage;
+    public TextView noPostsMessage;
+    public ListView postsListView;
+    public List<Post> postsArrayList;
+    ArrayAdapter<Post> arrayAdapter;
 
     // server side setup
     // 1. enable google authentication provider
@@ -54,38 +65,94 @@ public class FeedActivity extends AppCompatActivity {
     // 7. add support for the user logging out
 
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        listView = (ListView)
-                findViewById(R.id.listView);
-        chatMessageList = new ArrayList<>();
-        chatMessageList.add(new ChatMessage());
-        arrayAdapter = new ArrayAdapter<ChatMessage>(
-                this,
-                android.R.layout.simple_list_item_1,
-                chatMessageList
-        );
-        listView.setAdapter(arrayAdapter);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation_bar);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        postsListView = (ListView) findViewById(R.id.postsList);
+        noPostsMessage = (TextView) findViewById(R.id.noPostsText);
+
+        postsArrayList = new ArrayList<Post>();
+
+        // create array adapter to display title and description of posts
+        // will need to make a custom array adapter probably to display photo + post descriptions
+        arrayAdapter = new ArrayAdapter<Post>(this, android.R.layout.simple_list_item_2, android.R.id.text1, postsArrayList){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                text1.setText(postsArrayList.get(position).getTitle());
+                text2.setText(postsArrayList.get(position).getDescription());
+                return view;
+            }
+        };
+        // set array adapter
+        postsListView.setAdapter(arrayAdapter);
+
+        // set list item click listener to open post viewer activity
+        postsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent viewPost = new Intent(getApplicationContext(), PostViewerActivity.class);
+
+                // add post that was clicked to intent, then start post viewer activity
+                Post selectedPost = (Post) adapterView.getAdapter().getItem(position);
+                viewPost.putExtra("selectedPost", selectedPost);
+                startActivity(viewPost);
+            }
+        });
 
         // initialize the firebase references
         mFirebaseDatabase =
                 FirebaseDatabase.getInstance();
-        mMessagesDatabaseReference =
+        //TODO wire this for posts, not messages
+//        mMessagesDatabaseReference =
+//                mFirebaseDatabase.getReference()
+//                        .child("messages");
+
+        mPostDatabaseReference =
                 mFirebaseDatabase.getReference()
-                        .child("messages");
-        mMessagesChildEventListener = new ChildEventListener() {
+                    .child("posts");
+
+        mPostChildEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                // dataSnapshot stores the ChatMessage
-                ChatMessage chatMessage =
-                        dataSnapshot.getValue(ChatMessage.class);
+                // dataSnapshot stores the Post
+                Post post =
+                        dataSnapshot.getValue(Post.class);
                 // add it to the list, notify adapter
-                chatMessageList.add(chatMessage);
+                postsArrayList.add(post);
                 arrayAdapter.notifyDataSetChanged();
 
             }
@@ -111,6 +178,8 @@ public class FeedActivity extends AppCompatActivity {
             }
         };
 
+
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -121,7 +190,7 @@ public class FeedActivity extends AppCompatActivity {
                     setupUserSignedIn(user);
                 } else {
                     // user is signed out
-                    mMessagesDatabaseReference.removeEventListener(mMessagesChildEventListener);
+                    mPostDatabaseReference.removeEventListener(mPostChildEventListener);
                     Intent intent = AuthUI.getInstance()
                             .createSignInIntentBuilder()
                             .setIsSmartLockEnabled(false)
@@ -136,9 +205,16 @@ public class FeedActivity extends AppCompatActivity {
         };
     }
 
+    // when user clicks FAB to add new post, start intent for CreateNewPostActivity
+    public void addNewPost(View addButton) {
+        Intent goToNewPost = new Intent(this, CreateNewPostActivity.class);
+        startActivityForResult(goToNewPost, NEW_ITEM_REQUEST);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == SIGN_IN_REQUEST) {
             if (resultCode == Activity.RESULT_OK) {
                 Toast.makeText(this, "You are now signed in", Toast.LENGTH_SHORT).show();
@@ -146,6 +222,12 @@ public class FeedActivity extends AppCompatActivity {
             else if (resultCode == Activity.RESULT_CANCELED) {
                 finish();
             }
+        }
+
+        // when CreateNewPostActivity finishes, get the new post and add to feed list
+        if (requestCode == NEW_ITEM_REQUEST && resultCode == Activity.RESULT_OK) {
+            Post result = (Post) data.getSerializableExtra("new_post");
+            postsArrayList.add(result);
         }
     }
 
@@ -164,34 +246,15 @@ public class FeedActivity extends AppCompatActivity {
 
     private void setupUserSignedIn(FirebaseUser user) {
         userName = user.getDisplayName(); // get the user's name
-        mMessagesDatabaseReference
-                .addChildEventListener(mMessagesChildEventListener);
+        mPostDatabaseReference
+                .addChildEventListener(mPostChildEventListener);
     }
 
     private void setupUserSignedOut() {
         userName = "Anonymous";
-        chatMessageList.clear();
+        postsArrayList.clear();
         arrayAdapter.notifyDataSetChanged();
-        mMessagesDatabaseReference.removeEventListener(mMessagesChildEventListener);
-    }
-
-
-    public void onSendButtonClick(View view) {
-        EditText editText = (EditText)
-                findViewById(R.id.editText);
-        String currText = editText.getText().toString();
-        if (currText.isEmpty()) {
-            Toast.makeText(this, "Please enter a message first", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            ChatMessage chatMessage = new
-                    ChatMessage(userName,
-                    currText);
-            mMessagesDatabaseReference
-                    .push()
-                    .setValue(chatMessage);
-            editText.setText("");
-        }
+        mPostDatabaseReference.removeEventListener(mPostChildEventListener);
     }
 
     @Override
--- app/src/main/java/com/freshall/freshall/Model/Post.java
+++ app/src/main/java/com/freshall/freshall/Model/Post.java
@@ -1,62 +1,115 @@
 package com.freshall.freshall.Model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Post {
-    String postText;
-    Item postItem;
-    User postUser;
-    Date postTime;
-    Date updateTime;
+/**
+ * Created by angelarae on 10/28/18.
+ */
 
-    public Post(String postText, Item postItem, User postUser, Date postTime, Date updateTime) {
-        this.postText = postText;
-        this.postItem = postItem;
-        this.postUser = postUser;
-        this.postTime = postTime;
-        this.updateTime = updateTime;
+public class Post implements Serializable {
+    String title;
+    String description;
+    //    User seller;
+    String sellerName;
+    String location;
+    Double quantity;
+    String quantityType;
+    Double pricePerQuantity;
+    Date harvestDate;
+    Date expirationDate;
+
+    //DVC
+    public Post() {
+        this.title = "title";
+        this.description = "description";
+        this.sellerName = "seller_name";
+        this.location = "city, province, country";
+        this.quantity = 0.0;
+        this.quantityType = "each";
+        this.pricePerQuantity = 0.0;
+        this.harvestDate = new Date(2000, 1, 1);
+        this.expirationDate = new Date(2020, 1, 1);
     }
 
-    public String getPostText() {
-        return postText;
+    //EVC
+    public Post(String title, String description, String sellerName, String location, Double quantity, String quantityType, Double pricePerQuantity, Date harvestDate, Date expirationDate) {
+        this.title = title;
+        this.description = description;
+        this.sellerName = sellerName;
+        this.location = location;
+        this.quantity = quantity;
+        this.quantityType = quantityType;
     }
 
-    public void setPostText(String postText) {
-        this.postText = postText;
+    public String getTitle() {
+        return title;
     }
 
-    public Item getPostItem() {
-        return postItem;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public void setPostItem(Item postItem) {
-        this.postItem = postItem;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public User getPostUser() {
-        return postUser;
+    public String getDescription() {
+        return description;
     }
 
-    public void setPostUser(User postUser) {
-        this.postUser = postUser;
+    public String getSellerName() {return sellerName;}
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
     }
 
-    public Date getPostTime() {
-        return postTime;
+    public String getLocation() {
+        return location;
     }
 
-    public void setPostTime(Date postTime) {
-        this.postTime = postTime;
+    public void setLocation(String location) {
+        this.location = location;
     }
 
-    public Date getUpdateTime() {
-        return updateTime;
+    public Double getQuantity() {
+        return quantity;
     }
 
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
     }
 
-    //methods
-    void deletePost(){}
+    public String getQuantityType() {
+        return quantityType;
+    }
+
+    public void setQuantityType(String quantityType) {
+        this.quantityType = quantityType;
+    }
+
+    public Double getPricePerQuantity() {
+        return pricePerQuantity;
+    }
+
+    public void setPricePerQuantity(Double pricePerQuantity) {
+        this.pricePerQuantity = pricePerQuantity;
+    }
+
+    public Date getHarvestDate() {
+        return harvestDate;
+    }
+
+    public void setHarvestDate(Date harvestDate) {
+        this.harvestDate = harvestDate;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }

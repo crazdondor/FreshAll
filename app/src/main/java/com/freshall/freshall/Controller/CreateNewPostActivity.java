eeaa74a9ca90949fc070f8784e1e57b4e1ba495c
Bennett Falkenberg <bennettfalkenberg@gmail.com>
2019-02-07 23:32:29 -0800

    Added Location services, was gone for some reason

--- app/src/main/java/com/freshall/freshall/Controller/CreateNewPostActivity.java
+++ app/src/main/java/com/freshall/freshall/Controller/CreateNewPostActivity.java
@@ -2,7 +2,18 @@ package com.freshall.freshall.Controller;
 
 import com.freshall.freshall.R;
 import com.freshall.freshall.Model.Post;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +22,20 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class CreateNewPostActivity extends AppCompatActivity {
     boolean postHasTitle;
     Post selectedPost;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    static final int REQUEST_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +63,7 @@ public class CreateNewPostActivity extends AppCompatActivity {
             EditText priceEditor = (EditText) findViewById(R.id.price);
             priceEditor.setText(selectedPost.getPricePerQuantity());
 
-            EditText locationEditor = (EditText) findViewById(R.id.locationText);
+            TextView locationEditor = (TextView) findViewById(R.id.locationText);
             locationEditor.setText(selectedPost.getLocation());
 
             EditText quantityEditor = (EditText) findViewById(R.id.quantityNumber);
@@ -54,6 +74,28 @@ public class CreateNewPostActivity extends AppCompatActivity {
 //        Spinner quantityTypeEditor = (Spinner) findViewById(R.id.quantityType);
 //        quantityTypeEditor.setSelection(0);
 
+        }
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // location granted, get location
+            final TextView locationText = (TextView) findViewById(R.id.locationText);
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location == null) {
+                                locationText.setText("Null location");
+                            } else {
+                                setLocationText(location);
+                            }
+                        }
+                    });
+        } else {
+            // permission not granted, ask user
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
         }
     }
 
@@ -92,7 +134,7 @@ public class CreateNewPostActivity extends AppCompatActivity {
         }
 
         // if location text exists, add to new post
-        EditText locationEditor = (EditText) findViewById(R.id.locationText);
+        TextView locationEditor = (TextView) findViewById(R.id.locationText);
         String location = locationEditor.getText().toString();
         if (!location.equals("")) {
             newPost.setLocation(location);
@@ -122,6 +164,32 @@ public class CreateNewPostActivity extends AppCompatActivity {
             finish();
         } else {
             Toast.makeText(this, "Please enter title", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setLocationText(Location location) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses = null;
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    1);
+        } catch (IOException ioException) {
+            Log.e("Location", "Service not Available", ioException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Log.e("Location", "Bad Arguments", illegalArgumentException);
+        }
+
+        if (addresses == null || addresses.size() == 0) {
+            // no address found
+            Log.e("Location", "No addresses found");
+        } else {
+            // set location TextView
+            TextView locationText = (TextView) findViewById(R.id.locationText);
+            locationText.setText((CharSequence) addresses.get(0).getLocality());
         }
     }
 }

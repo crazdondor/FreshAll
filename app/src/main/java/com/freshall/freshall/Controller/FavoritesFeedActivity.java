afdb8fd39d3b434dd2bbf547e2dcaea820b28758
angelalrae <angelarae512@gmail.com>
2019-04-22 12:58:22 -0700

    using uuid to get post, need to convert Object to Post to display

--- app/src/main/java/com/freshall/freshall/Controller/FavoritesFeedActivity.java
+++ app/src/main/java/com/freshall/freshall/Controller/FavoritesFeedActivity.java
@@ -1,19 +1,12 @@
 package com.freshall.freshall.Controller;
 
 import android.content.Intent;
-import android.renderscript.Sampler;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.freshall.freshall.Model.Post;
@@ -24,15 +17,12 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class FavoritesFeedActivity extends AppCompatActivity {
     private User user;
@@ -44,13 +34,20 @@ public class FavoritesFeedActivity extends AppCompatActivity {
     public FirebaseDatabase mFirebaseDatabase;
     public DatabaseReference mFavoritesDatabaseReference;
     public DatabaseReference mPostDatabaseReference;
+    public final Query mFavoritesQuery = FirebaseDatabase.getInstance()
+            .getReference()
+            .child("favorites");
     public final Query mPostsQuery = FirebaseDatabase.getInstance()
             .getReference()
             .child("posts");
 
     ArrayList<Post> postsArrayList;
-    PostsArrayAdapter arrayAdapter;
+    PostsArrayAdapter postsArrayAdapter;
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,27 +61,25 @@ public class FavoritesFeedActivity extends AppCompatActivity {
         }
         catch (Exception e) {
             Toast.makeText(this, "No user available", Toast.LENGTH_SHORT).show();
+            finish();
         }
 
-        // get references to views
+        // get references to view
         ListView postsListView = (ListView) findViewById(R.id.postsList);
-        TextView noPostsMessage = (TextView) findViewById(R.id.noPostsText);
 
         // initialize posts array list
         postsArrayList = new ArrayList<>();
 
         // create array adapter to display title and description of posts
-        arrayAdapter = new PostsArrayAdapter(this, postsArrayList);
+        postsArrayAdapter = new PostsArrayAdapter(this, postsArrayList);
 
-        // set array adapter
-        postsListView.setAdapter(arrayAdapter);
-
+        // set array adapter in list view
+        postsListView.setAdapter(postsArrayAdapter);
 
         // set list item click listener to open post viewer activity
         postsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Log.i("listview", "listview item clicked");
                 Intent viewPost = new Intent(getApplicationContext(), PostViewerActivity.class);
 
                 // add post that was clicked to intent, then start post viewer activity
@@ -98,63 +93,60 @@ public class FavoritesFeedActivity extends AppCompatActivity {
         // get references to Firebase favorites database
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFavoritesDatabaseReference = mFirebaseDatabase.getReference().child("favorites");
-//        mPostDatabaseReference = mFirebaseDatabase.getReference().child("posts");
+        mPostDatabaseReference = mFirebaseDatabase.getReference().child("posts");
+
+        //
+        mFavoritesQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+                Log.d(TAG, "onDataChange: post = " + post.getDescription());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // getting post failed, log a messageLog.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                Toast.makeText(FavoritesFeedActivity.this, "Failed to load post",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // define database listener for items added to favorites
         mPostChildEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                // dataSnapshot stores the Post
-                HashMap<String, Object> favorites_entry = (HashMap<String, Object>) dataSnapshot.getValue();
+                // dataSnapshot stores the favorites entry
+                HashMap<String, String> favorites_entry = (HashMap<String, String>) dataSnapshot.getValue();
 
-                Log.d(TAG, "onChildAdded: " + favorites_entry.entrySet());
                 // if favorites post belongs to current user, get the post
                 // add post to the list, notify adapter
                 if (favorites_entry.keySet().contains(userName)){
-                    Collection values_collection = favorites_entry.values();
-                    Object[] values = values_collection.toArray();
+                    // get reference to values for given key
+                    Collection<String> values_collection = favorites_entry.values();
 
-                    for (Object value : values) {
-                        final String value_string = value.toString();
+                    //values_collection should have one object (a post)
+                    for (String value : values_collection) {
+                        try {
+                            DatabaseReference reference = FirebaseDatabase.getInstance()
+                                    .getReference()
+                                    .child(value).child("description");
 
-                        Query query = mFirebaseDatabase.getReference().child("posts").orderByKey().equalTo(value_string);
-                        query.addChildEventListener(new ChildEventListener() {
-                            @Override
-                            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                                Log.d(TAG, "onChildAdded: " + dataSnapshot.child(value_string));
-                                Log.d(TAG, "onChildAdded: " + mPostsQuery);
-//                                Post post = (Post) dataSnapshot;
-                            }
+                            Log.d(TAG, "onChildAdded: " + FirebaseDatabase.getInstance()
+                                    .getReference()
+                                    .child(value).child("description"));
 
-                            @Override
-                            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-                            }
-
-                            @Override
-                            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                            }
-
-                            @Override
-                            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                            }
-
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-
-                            }
-                        });
+//                            postsArrayList.add(post);
+                            postsArrayAdapter.notifyDataSetChanged();
+                        }
+                        catch (Exception e){
+                            Log.d(TAG, "onChildAdded: exception" + e);
+                        }
 
                     }
 
                 }
-//                else {
-//
-//                }
-//                postsArrayList.add(post);
-                arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -178,8 +170,13 @@ public class FavoritesFeedActivity extends AppCompatActivity {
         };
 
         // attach listener
-        mFavoritesDatabaseReference.addChildEventListener(mPostChildEventListener);
-//        mPostsQuery.addChildEventListener(mPostChildEventListener);
+        mFavoritesQuery.addChildEventListener(mPostChildEventListener);
     }
+
+//    private Post objectToPost(Object object) {
+//        Post new_post = new Post();
+//        Log.d(TAG, "objectToPost: " + object.toString());
+//        return new_post;
+//    }
 
 }
